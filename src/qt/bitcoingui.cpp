dda781bc129e032f66ec3be4cb7819f502f88c00
James Lovejoy <jameslovejoy1@gmail.com>
2017-02-19 23:54:22 -0500

    Deprecate stealth addresses

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -200,12 +200,12 @@ void BitcoinGUI::createActions()
     addressBookAction->setShortcut(QKeySequence(Qt::ALT + Qt::Key_5));
     tabGroup->addAction(addressBookAction);
 
-    stealthAddressAction = new QAction(QIcon(":/icons/stealth_addresses"), tr("&Stealth Addresses"), this);
+    /*stealthAddressAction = new QAction(QIcon(":/icons/stealth_addresses"), tr("&Stealth Addresses"), this);
     stealthAddressAction->setStatusTip(tr("Show the list of stealth addresses for receiving payments"));
     stealthAddressAction->setToolTip(stealthAddressAction->statusTip());
     stealthAddressAction->setCheckable(true);
     stealthAddressAction->setShortcut(QKeySequence(Qt::ALT + Qt::Key_6));
-    tabGroup->addAction(stealthAddressAction);
+    tabGroup->addAction(stealthAddressAction);*/
 
     connect(overviewAction, SIGNAL(triggered()), this, SLOT(showNormalIfMinimized()));
     connect(overviewAction, SIGNAL(triggered()), this, SLOT(gotoOverviewPage()));
@@ -217,8 +217,8 @@ void BitcoinGUI::createActions()
     connect(historyAction, SIGNAL(triggered()), this, SLOT(gotoHistoryPage()));
     connect(addressBookAction, SIGNAL(triggered()), this, SLOT(showNormalIfMinimized()));
     connect(addressBookAction, SIGNAL(triggered()), this, SLOT(gotoAddressBookPage()));
-    connect(stealthAddressAction, SIGNAL(triggered()), this, SLOT(showNormalIfMinimized()));
-    connect(stealthAddressAction, SIGNAL(triggered()), this, SLOT(gotoStealthAddressPage()));
+    //connect(stealthAddressAction, SIGNAL(triggered()), this, SLOT(showNormalIfMinimized()));
+    //connect(stealthAddressAction, SIGNAL(triggered()), this, SLOT(gotoStealthAddressPage()));
 
     quitAction = new QAction(QIcon(":/icons/quit"), tr("E&xit"), this);
     quitAction->setStatusTip(tr("Quit application"));
@@ -301,7 +301,7 @@ void BitcoinGUI::createToolBars()
     toolbar->addAction(overviewAction);
     toolbar->addAction(sendCoinsAction);
     toolbar->addAction(receiveCoinsAction);
-    toolbar->addAction(stealthAddressAction);
+    //toolbar->addAction(stealthAddressAction);
     toolbar->addAction(historyAction);
     toolbar->addAction(addressBookAction);
 }
@@ -380,7 +380,7 @@ void BitcoinGUI::setWalletActionsEnabled(bool enabled)
     signMessageAction->setEnabled(enabled);
     verifyMessageAction->setEnabled(enabled);
     addressBookAction->setEnabled(enabled);
-    stealthAddressAction->setEnabled(enabled);
+    //stealthAddressAction->setEnabled(enabled);
 
 }
 
